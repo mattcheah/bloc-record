50a3bbea693d61dc5a73af3cd8deb5a8737ea59b
Matt Cheah <matt.cheah@gmail.com>
2017-06-30 22:47:16 +0000

    add input validation and make find_by() dynamic using method_missing

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -1,23 +1,131 @@
 require 'sqlite3'
 
 module Selection
-    def find(id)
-        row = connection.get_first_row <<-SQL
-            SELECT #{columns.join ","} FROM #{table}
-            WHERE id = #{id};
-        SQL
-        
-        data = Hash[columns.zip(row)]
-        new(data)
+    
+    def find(*ids)
+        if ids.all? { |i| i.is_a?(Integer) && i > 0}
+            if ids.length == 1
+                find_one(ids.first)
+            else
+                rows = connection.execute <<-SQL
+                    SELECT #{columns.join ","} FROM #{table}
+                    WHERE id IN (#{ids.join(",")});
+                SQL
+                
+                rows_to_array(rows)
+            
+            end
+        else
+            puts "Error: All arguments must be positive integers."
+        end
+    end
+    
+    def find_one(id)
+        if id.is_a?(Integer) && id > 0
+            row = connection.get_first_row <<-SQL
+                SELECT #{columns.join ","} FROM #{table}
+                WHERE id = #{id};
+            SQL
+            
+            init_object_from_row(row)
+        else
+            puts "Error: id must be a positive integer."
+        end
     end
     
     def find_by(attribute, value)
-        row = connection.execute(<<-SQL)
-            SELECT * FROM #{table}
-            WHERE #{attribute} = #{value};
+        if columns.include?(attribute)
+        
+            row = connection.execute(<<-SQL)
+                SELECT #{columns.join ","} FROM #{table}
+                WHERE #{attribute} = #{BlocRecord::Utility.sql_strings(value)};
+            SQL
+            
+            init_object_from_row(row)
+        
+        else
+            puts "Error: `#{attribute}` is not a column name in this table."
+        end
+    end
+    
+    def take(num=1)
+        if num >= 1 && num.is_a?(Integer)
+            if num == 1 
+                take_one
+            else
+                rows = connection.execute <<-SQL
+                    SELECT #{columns.join ","} FROM #{table}
+                    ORDER BY random()
+                    LIMIT #{num}
+                SQL
+                
+                rows_to_array(rows)
+            end
+        else 
+            puts "Error: Number of records to retrieve must be a positive integer. (You gave: #{num}"
+        end
+    end
+    
+    def take_one
+        row = connection.get_first_row <<-SQL
+            SELECT #{columns.join ","} FROM #{table}
+            ORDER BY random()
+            LIMIT 1;
         SQL
         
-        data = Hash[columns.zip(row)]
-        new(data)
+        init_object_from_row(row)
     end
+    
+    def first 
+        row = connection.execute <<-SQL
+            SELECT #{columns.join ","} FROM #{table}
+            ORDER BY id ASC
+            LIMIT 1
+        SQL
+        
+        init_object_from_row(row)
+    end
+    
+    def last
+        row = connection.execute <<-SQL
+            SELECT #{columns.join ","} FROM #{table}
+            ORDER BY id DESC
+            LIMIT 1
+        SQL
+        
+        init_object_from_row(row)
+    end
+    
+    def all
+        rows = connection.execute <<-SQL
+            SELECT #{columns.join ","} FROM #{table}
+        SQL
+        
+        rows_to_array(rows)
+    end
+    
+    def method_missing(m, *args, &block)
+    
+        if m[0..7] == "find_by_"
+            find_attribute = m[8..m.length-1]
+            find_by(find_attribute, args[0])
+        else
+            puts "There's no method called #{m} here -- please try again."  
+        end
+    
+    end
+    
+    private
+    
+    def init_object_from_row(row)
+        if row
+            data = Hash[columns.zip(row)]
+            new(data)
+        end
+    end
+    
+    def rows_to_array(rows)
+        rows.map {|row| new(Hash[columns.zip(row)]) }
+    end
+    
 end
--- testing/test.rb
+++ testing/test.rb
@@ -1,8 +1,19 @@
-def convert(my_string) 
-
-    my_string.gsub!(/_./) {|x| x[1].upcase }
-    
+ def find(*ids)
+    if ids.all? { |i| i.is_a?(Integer) && i > 0}
+        if ids.length == 1
+            find_one(ids.first)
+        else
+            puts ids.join(",")
+        end
+    else
+        puts "Error: All arguments must be positive integers."
+    end
 end
 
-puts convert("hello_there_bud")
-puts convert("hello_there_bud__HEY")
+puts "finding: (1,2,3,4,5)"
+find(1,2,3,4,5)
+puts "finding: (1,2,'red','soap')"
+find(1,2,'red', 'soap')
+puts "finding: (1,2,{name: 'matt'})"
+find(1,2,{name: 'matt'})
+find(-2, 5, 1, 0)
